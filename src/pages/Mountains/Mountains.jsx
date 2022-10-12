964bb826417dc1793548f086d11138792d8e68ce
[James] <[james.hinton@live.co.uk]>
2022-10-13 00:55:40 +0100

    added epic mountains

--- src/pages/Mountains/Mountains.jsx
+++ src/pages/Mountains/Mountains.jsx
@@ -1,4 +1,5 @@
 // this will be a nice page with mountains on it :)
+import { blue } from "@mui/material/colors";
 import { useState } from "react";
 import "./Mountains.scss";
 
@@ -10,23 +11,29 @@ const Mountains = () => {
       id: 1,
       src: "/mountains/landscape.png",
       alt: "mountain landscape",
-      moveSpeed: 0.7,
+      moveSpeed: 0.1,
       zIndex: 5,
+      className: "landscape",
+      blurSpeed: 0.02,
+      blur: 0,
     },
     {
       id: 2,
       src: "/mountains/dark.png",
       alt: "mountain dark",
-      moveSpeed: 0.2,
+      moveSpeed: -0.25,
       zIndex: 2,
+      className: "dark",
+      blurSpeed: -0.1,
+      blur: 4,
     },
     {
       id: 3,
       src: "/mountains/eagle.png",
       alt: "eagle",
-      moveSpeed: 1.2,
+      moveSpeed: 0.8,
       moveLeft: true,
-      zIndex: 5,
+      zIndex: 3,
       height: "30vh",
       right: "-30%",
       className: "eagle",
@@ -53,11 +60,39 @@ const Mountains = () => {
           window.pageYOffset * speed
         }px)`;
       }
+
+      // Blur the mountains as we scroll down
+      if (relativeMountain.blurSpeed) {
+        let blur = window.pageYOffset * relativeMountain.blurSpeed;
+        blur = Math.round((blur + Number.EPSILON) * 100) / 100;
+        if (blur < 0.5) {
+          blur = 0;
+        }
+        mountain.style.filter = `blur(${blur}px)`;
+      }
     });
+  };
+
+  // If at top of the page, reset the mountains to their original position
+  const handleReset = () => {
+    // if at top of page, reset the mountains
+    if (window.pageYOffset === 0) {
+      let mountainsElements = document.querySelectorAll(".mountain");
+
+      mountainsElements.forEach((mountain) => {
+        let relativeMountain = mountains.find(
+          (m) => m.id === parseInt(mountain.id)
+        );
+
+        // Mountain filter
+        mountain.style.filter = `blur(${relativeMountain.blur}px)`;
+      });
+    }
   };
 
   // Add the event listener
   window.addEventListener("scroll", handleScroll);
+  window.addEventListener("scroll", handleReset);
 
   return (
     <>
@@ -74,6 +109,7 @@ const Mountains = () => {
               height: mountain.height,
               position: "absolute",
               right: mountain.right,
+              filter: `blur(${mountain.blur}px)`,
             }}
           />
         ))}
