2e07f2c9195f3fbd8880d8c464fac2c8595077ef
James <james.hinton@live.co.uk>
2022-10-22 19:35:16 +0100

    changed audio playback from bing

--- src/components/Zhongwen/SavedWords/components/Modal/index.jsx
+++ src/components/Zhongwen/SavedWords/components/Modal/index.jsx
@@ -14,43 +14,15 @@ const Modal = ({ openModal, setOpenModal, data }) => {
     const audioFile = `https://www.mdbg.net/chinese/rsc/audio/voice_pinyin_pz/${pinyin}.mp3`;
     return audioFile;
   };
-  const getFullAudioFile = async (characters) => {
-    const resp = await fetch(
-      "https://westeurope.tts.speech.microsoft.com/cognitiveservices/v1?",
-      {
-        method: "POST",
-        headers: {
-          authority: "westeurope.tts.speech.microsoft.com",
-          accept: "*/*",
-          "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-          authorization:
-            "Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJyZWdpb24iOiJ3ZXN0ZXVyb3BlIiwic3Vic2NyaXB0aW9uLWlkIjoiZTExODFkYTI2YzhhNGQ5ZDlkYjcwOWM0YzkwNjQxYTciLCJwcm9kdWN0LWlkIjoiQ29nbml0aXZlU2VydmljZXMuUzAiLCJjb2duaXRpdmUtc2VydmljZXMtZW5kcG9pbnQiOiJodHRwczovL2FwaS5jb2duaXRpdmUubWljcm9zb2Z0LmNvbS9pbnRlcm5hbC92MS4wLyIsImF6dXJlLXJlc291cmNlLWlkIjoiL3N1YnNjcmlwdGlvbnMvMTUzYTFlMTEtYTU5Yi00M2IxLTk5ZWUtNGI2NmVlMjk1ZDljL3Jlc291cmNlR3JvdXBzL1Byb2RFc3NlbnRpYWxzL3Byb3ZpZGVycy9NaWNyb3NvZnQuQ29nbml0aXZlU2VydmljZXMvYWNjb3VudHMvU1RDSVRyYW5zbGF0aW9uQW5zd2VyTmV1cmFsV2VzdEV1cm9wZSIsInNjb3BlIjpbInNwZWVjaHRvaW50ZW50cyIsImh0dHBzOi8vYXBpLm1pY3Jvc29mdHRyYW5zbGF0b3IuY29tLyIsInNwZWVjaHNlcnZpY2VzIiwidmlzaW9uIl0sImF1ZCI6WyJ1cm46bXMuc3BlZWNoIiwidXJuOm1zLmx1aXMud2VzdGV1cm9wZSIsInVybjptcy5taWNyb3NvZnR0cmFuc2xhdG9yIiwidXJuOm1zLnNwZWVjaHNlcnZpY2VzLndlc3RldXJvcGUiLCJ1cm46bXMudmlzaW9uLndlc3RldXJvcGUiXSwiZXhwIjoxNjY2NDYyODI4LCJpc3MiOiJ1cm46bXMuY29nbml0aXZlc2VydmljZXMifQ.xDVosWo4EVZs_rnjIt28D8Py1RW2HAmHh0LTi9Fck1M",
-          "cache-control": "no-cache",
-          "content-type": "application/ssml+xml",
-          origin: "https://www.bing.com",
-          pragma: "no-cache",
-          referer: "https://www.bing.com/",
-          "sec-ch-ua":
-            '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": '"Linux"',
-          "sec-fetch-dest": "empty",
-          "sec-fetch-mode": "cors",
-          "sec-fetch-site": "cross-site",
-          "user-agent":
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-          "x-microsoft-outputformat": "audio-16khz-32kbitrate-mono-mp3",
-        },
-        body: `<speak version='1.0' xml:lang='zh-CN'><voice xml:lang='zh-CN' xml:gender='Female' name='zh-CN-XiaoxiaoNeural'><prosody rate='-20.00%'>
-        {${characters}}
-        </prosody></voice></speak>`,
-      }
-    );
-    // Convert characters to unicode
-
-    const blob = await resp.blob();
-    const audioFile = URL.createObjectURL(blob);
-    return audioFile;
+  const getFullAudioFile = async (pinyin) => {
+    // Loop through each pinyin and get audio file
+    const pinyinArray = pinyin.split(" ");
+    const audioFiles = [];
+    for (let i = 0; i < pinyinArray.length; i++) {
+      const audioFile = getAudioFile(pinyinArray[i]);
+      audioFiles.push(audioFile);
+    }
+    return audioFiles;
   };
   return (
     <>
@@ -67,10 +39,7 @@ const Modal = ({ openModal, setOpenModal, data }) => {
               <div className="zhongwen-modal-body-content">
                 {/* Pinyin */}
                 <div className="zhongwen-modal-body-content-row">
-                  <div
-                    className="zhongwen-modal-body-content-row-label"
-
-                  >
+                  <div className="zhongwen-modal-body-content-row-label">
                     Pinyin
                   </div>
                   <div
@@ -79,10 +48,19 @@ const Modal = ({ openModal, setOpenModal, data }) => {
                       cursor: "pointer",
                     }}
                     onClick={async (e) => {
-                      // Get audio for whole sentence
-                      const audioFile = await getFullAudioFile(data.hanzi);
-                      const audio = new Audio(audioFile);
-                      audio.play();
+                      // Get array of audio files
+                      const audioFile = await getFullAudioFile(data.pinyin);
+                      // Loop through each audio file and play
+                      for (let i = 0; i < audioFile.length; i++) {
+                        const audio = new Audio(audioFile[i]);
+                        audio.play();
+
+                        // Wait for audio to finish playing before playing next audio file
+                        await new Promise((resolve) =>
+                          audio.addEventListener("ended", resolve)
+                        );
+                        
+                      }
                     }}
                   >
                     {data.pinyin}
