bc7b333cf095512d357f245285d2cfb30e1187a8
James <james.hinton@live.co.uk>
2022-10-30 20:02:11 +0000

    fixed viewport bug

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,24 +4,10 @@ import Swiper from "../../components/Landing/Swiper/Swiper";
 import Footer from "../../components/Generic/Footer/Footer";
 import Cursor from "../../components/Generic/Cursor/Cursor";
 import { useMediaQuery } from "@mui/material";
-import { useEffect, useState } from "react";
 
 const Landing = ({ pokeData, activeCursor, showContent }) => {
   // Is mobile check if screen is less than 1000px
-  const [isMobile, setIsMobile] = useState(false);
-
-  // Whenever window is resized, check if it is mobile
-  window.addEventListener("resize", () => {
-    if (window.innerWidth < 1000) {
-      console.log('Setting isMobile to true');
-      setIsMobile(true);
-    }
-    else {
-      console.log('Setting isMobile to false');
-      setIsMobile(false);
-    }
-  });
-
+  const isMobile = useMediaQuery("(width:1000px)");
 
   console.log("isMobile", isMobile);
   return (
