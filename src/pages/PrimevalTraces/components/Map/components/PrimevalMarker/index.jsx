fb083a45e1080af2c9255f6ee1ef52fd12c328de
James Hinton <james.hinton@live.co.uk>
2023-05-13 16:25:33 +0100

    feat: custom icon support

--- src/pages/PrimevalTraces/components/Map/components/PrimevalMarker/index.jsx
+++ src/pages/PrimevalTraces/components/Map/components/PrimevalMarker/index.jsx
@@ -1,12 +1,23 @@
+import L from 'leaflet';
 import { Marker, Popup, Icon } from "react-leaflet";
 import  FossilIcon from "../../../../assets/fossil-1.svg";
 
-const PrimevalMarker = ({ position }) => {
+const PrimevalMarker = ({ position, content }) => {
+
+  const icon = new L.Icon({
+    iconUrl: FossilIcon,
+    iconRetinaUrl: FossilIcon,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+    shadowSize: [68, 95],
+    shadowAnchor: [20, 92],
+  });
 
   return (
-    <Marker position={position}>
+    <Marker position={position} icon={icon}>
       <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
+        {content}
       </Popup>
     </Marker>
   );
--- src/pages/PrimevalTraces/components/Map/index.jsx
+++ src/pages/PrimevalTraces/components/Map/index.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { MapContainer, TileLayer, useMap} from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import PrimevalMarker from "./components/PrimevalMarker";
 
 import "leaflet/dist/leaflet.css";
@@ -15,7 +15,7 @@ const PrimevalMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <PrimevalMarker position={[51.505, -0.09]}/>
+        <PrimevalMarker position={[51.505, -0.09]} content="Dinosaur Fossil" />
       </MapContainer>
     </div>
   );
